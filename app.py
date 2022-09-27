e7e7dc6d89e6d2aa7089ddc79ddf9fecc91fb9d4
Ankeeta Sahoo <sahooankeeta@gmail.com>
2022-09-27 20:11:45 +0530

    test

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-import numpy as np
-from flask import Flask,request, url_for, redirect, jsonify
+import requests
+from flask import Flask,request,render_template,json
+import trained_model as m
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import pickle
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import numpy as np
 
 app = FastAPI()
 
@@ -15,25 +19,39 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
-class_to_index={'fear': 0, 'joy': 1, 'surprise': 2, 'sadness': 3, 'anger': 4, 'love': 5}
+model=pickle.load(open('tweet_emotion_model','rb'))
+tokenizer=Tokenizer(num_words=10000,oov_token='<UNK>')
+maxlen=60
+class_to_index={'fear': 0, 'love': 1, 'sadness': 2, 'anger': 3, 'surprise': 4, 'joy': 5}
 index_to_class=dict((v,k) for k,v in class_to_index.items())
 
-tweet_emotion_model=pickle.load(open('tweet_emotion_model.sav','rb'))
+names_to_ids=lambda labels: np.array([class_to_index.get(x) for x in labels])
 
-@app.route("/")
-def home():
-  return "hi there"
+def get_sequence(tokenizer,tweets):
+  sequences=tokenizer.texts_to_sequences(tweets)
+  padded=pad_sequences(sequences,truncating='post',padding='post',maxlen=maxlen)
+  return padded
+# app = Flask(__name__)
 
-@app.route('/emotion_prediction',methods=['POST'])
-def emotion_pred():
-  print(request.form)
-  input_data=request.form.tweet
-  # input_dict=json.loads(input_data)
-  # tweet=input_dict['tweet']
-  # p=tweet_emotion_model.predict(np.expand_dims(tweet,axis=0))[0]
+# @app.route("/")
+# def home():
+#   return render_template("home.html")
 
-  # pred_class=index_to_class[np.argmax(p).astype('uint8')]
-  return input_data
 
+@app.post("/predict")
+def submit():
+  r=requests.json()
+#   #r=requests.post('http://127.0.0.1:5000/test',data={'t':request.form['tweet']})
+  text=r['tweet']
+  t=get_sequence(tokenizer,[text])
+  p=model.predict(np.expand_dims(t[0],axis=0))[0]
+  pred_class=index_to_class[np.argmax(p).astype('uint8')]
+  return pred_class
+#   return render_template("home.html",t=ex)
+
+# # @app.route("/test",methods=['POST'])
+# # def testing():
+# #    return request.form
+   
+# if __name__=='main':
+#   app.run()
---
+++ trained_model.py
@@ -0,0 +1,76 @@
+# import tensorflow as tf
+# import numpy as np
+# import nlp
+# from tensorflow.keras.preprocessing.text import Tokenizer
+# from tensorflow.keras.preprocessing.sequence import pad_sequences
+# import pickle
+# dataset=nlp.load_dataset('emotion')
+
+# train=dataset['train']
+# test=dataset['test']
+# val=dataset['validation']
+
+# def get_tweet(data):
+#   tweets=[x['text'] for x in data]
+#   labels=[x['label'] for x in data]
+#   return tweets,labels
+
+# tweets,labels=get_tweet(train)
+
+
+# tokenizer=Tokenizer(num_words=10000,oov_token='<UNK>')
+# tokenizer.fit_on_texts(tweets)
+
+# maxlen=60
+
+# def get_sequence(tokenizer,tweets):
+#   sequences=tokenizer.texts_to_sequences(tweets)
+#   padded=pad_sequences(sequences,truncating='post',padding='post',maxlen=maxlen)
+#   return padded
+
+# padded_train_seq=get_sequence(tokenizer,tweets)
+
+# classes=set(labels)
+# class_to_index={'fear': 0, 'love': 1, 'sadness': 2, 'anger': 3, 'surprise': 4, 'joy': 5}
+# index_to_class=dict((v,k) for k,v in class_to_index.items())
+
+# names_to_ids=lambda labels: np.array([class_to_index.get(x) for x in labels])
+
+# train_labels=names_to_ids(labels)
+
+# model=tf.keras.models.Sequential([
+#     tf.keras.layers.Embedding(10000,16,input_length=maxlen),
+#     tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(20,return_sequences=True)),
+#     tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(20)),
+#     tf.keras.layers.Dense(6,activation='softmax')
+# ])
+# model.compile(
+#     loss='sparse_categorical_crossentropy',
+#     optimizer='adam',
+#     metrics=['accuracy']
+# )
+
+# val_tweets,val_labels=get_tweet(val)
+# val_seq=get_sequence(tokenizer,val_tweets)
+# val_labels=names_to_ids(val_labels)
+
+# h=model.fit(
+#     padded_train_seq,train_labels,
+#     validation_data=(val_seq,val_labels),
+#     epochs=20,
+#     callbacks=[
+#         tf.keras.callbacks.EarlyStopping(monitor='val_accuracy',patience=2)
+#     ]
+# )
+
+# test_tweets,test_labels=get_tweet(test)
+# test_seq=get_sequence(tokenizer,test_tweets)
+# test_labels=names_to_ids(test_labels)
+
+# _=model.evaluate(test_seq,test_labels)
+# model=pickle.load(open('tweet_emotion_model','rb'))
+# def predict(tweet):
+#   t=get_sequence(tokenizer,[tweet])
+#   p=model.predict(np.expand_dims(t[0],axis=0))[0]
+#   pred_class=index_to_class[np.argmax(p).astype('uint8')]
+#   return pred_class
