09a5ac2242a0ece31bdd18e12ac5d525c2b8fb3c
Ankeeta Sahoo <sahooankeeta@gmail.com>
2022-09-27 23:58:26 +0530

    'checxk'

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-
 import requests
-from flask import Flask
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-import pickle
+from flask import Flask,json
+import os.path
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model
 import numpy as np
-
+# import tweet_model
 app = Flask(__name__)
 
 
-model=pickle.load(open('tweet_emotion_model','rb'))
+
+model=load_model('tweet_emotion_model.h5')
 tokenizer=Tokenizer(num_words=10000,oov_token='<UNK>')
 maxlen=60
 class_to_index={'fear': 0, 'love': 1, 'sadness': 2, 'anger': 3, 'surprise': 4, 'joy': 5}
 index_to_class=dict((v,k) for k,v in class_to_index.items())
+filename='tweet_emotion_model.h5'
 
 names_to_ids=lambda labels: np.array([class_to_index.get(x) for x in labels])
 
@@ -30,8 +30,9 @@ def home():
 
 @app.route("/predict",methods=['POST'])
 def submit():
-  r=requests.json()
-  text=r['tweet']
+  input_data =requests.response()
+  input_dictionary = input_data.json()
+  text=input_dictionary['tweet']
   t=get_sequence(tokenizer,[text])
   p=model.predict(np.expand_dims(t[0],axis=0))[0]
   pred_class=index_to_class[np.argmax(p).astype('uint8')]
